e3e8e1457488f579727d8e3d76d3378c4154e850
20201tiimi0322 <gileard96@gmail.com>
2022-09-02 09:57:03 -0300

    Comecei novo projeto

--- app/src/main/java/barros/gileardy/galeria/PhotoActivity.java
+++ app/src/main/java/barros/gileardy/galeria/PhotoActivity.java
@@ -1,6 +1,8 @@
 package barros.gileardy.galeria;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
